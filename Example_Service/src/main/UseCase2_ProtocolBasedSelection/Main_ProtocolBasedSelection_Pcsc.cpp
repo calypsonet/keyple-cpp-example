12166db88ce9bed98b8cc1228c9ec18f3dcc9b4f
Alexandre MUNSCH <alexandre.munsch@gmail.com>
2021-12-05 17:52:22 +0100

    Feat: completed UseCase2_ProtocolBasedSelection

--- Example_Service/src/main/UseCase2_ProtocolBasedSelection/Main_ProtocolBasedSelection_Pcsc.cpp
+++ Example_Service/src/main/UseCase2_ProtocolBasedSelection/Main_ProtocolBasedSelection_Pcsc.cpp
@@ -11,14 +11,14 @@
  **************************************************************************************************/
 
 /* Keyple Card Generic */
+#include "GenericCardSelectionAdapter.h"
 #include "GenericExtensionService.h"
 
-/* Keyple Core Util */
-#include "ByteArrayUtil.h"
-#include "IllegalStateException.h"
+ /* Keyple Core Util */
 #include "LoggerFactory.h"
 
 /* Keyple Core Service */
+#include "ConfigurableReader.h"
 #include "SmartCardService.h"
 #include "SmartCardServiceProvider.h"
 
@@ -30,41 +30,43 @@
 
 using namespace keyple::card::generic;
 using namespace keyple::core::service;
-using namespace keyple::core::util;
 using namespace keyple::core::util::cpp;
-using namespace keyple::core::util::cpp::exception;
 using namespace keyple::plugin::pcsc;
 
 /**
  *
  *
- * <h1>Use Case Generic 1 – Basic Selection (PC/SC)</h1>
+ * <h1>Use Case Generic 2 – Protocol Based Selection (PC/SC)</h1>
  *
- * <p>We demonstrate here a selection of cards without any condition related to the card itself. Any
- * card able to communicate with the reader must lead to a "selected" state.<br>
- * Note that in this case, no APDU "select application" is sent to the card.<br>
- * However, upon selection, an APDU command specific to Global Platform compliant cards is sent to
- * the card and may fail depending on the type of card presented.
+ * <p>We demonstrate here a selection of cards with the only condition being the type of
+ * communication protocol they use, in this case the Mifare Classic. Any card of the Mifare Classic
+ * type must lead to a "selected" status, any card using another protocol must be ignored.<br>
+ * Note that in this case, no APDU "select application" is sent to the card.
  *
  * <h2>Scenario:</h2>
  *
  * <ul>
- *   <li>Check if a ISO 14443-4 card is in the reader, select a card (a GlobalPlatform compliant
- *       card is expected here [e.g. EMV card or Javacard]).
- *   <li>Run a selection scenario without filter.
+ *   <li>Check if a ISO 14443-4 card is in the reader, select a card (a Mifare Classic card is
+ *       expected here).
+ *   <li>Run a selection scenario with the MIFARE CLASSIC protocol filter.
  *   <li>Output the collected smart card data (power-on data).
- *   <li>Send a additional APDUs to the card (get Card Production Life Cycle data [CPLC]).
  * </ul>
+ *
+ * All results are logged with slf4j.
  *
  * <p>Any unexpected behavior will result in runtime exceptions.
  *
  * @since 2.0.0
  */
-class Main_BasicSelection_Pcsc {};
-const std::unique_ptr<Logger> logger = LoggerFactory::getLogger(typeid(Main_BasicSelection_Pcsc));
+class Main_ProtocolBasedSelection_Pcsc {};
+const std::string MIFARE_CLASSIC = "MIFARE_CLASSIC";
+const std::unique_ptr<Logger> logger = 
+    LoggerFactory::getLogger(typeid(Main_ProtocolBasedSelection_Pcsc));
 
 int main()
 {
+    logger->setLoggerLevel(Logger::Level::logTrace);
+
     /* Get the instance of the SmartCardService (singleton pattern) */
     SmartCardService& smartCardService = SmartCardServiceProvider::getService();
 
@@ -85,7 +87,11 @@ int main()
     std::shared_ptr<Reader> reader =
         ConfigurationUtil::getCardReader(plugin, ConfigurationUtil::CONTACTLESS_READER_NAME_REGEX);
 
-    logger->info("=============== UseCase Generic #1: basic card selection ==================\n");
+    std::dynamic_pointer_cast<ConfigurableReader>(reader) 
+        ->activateProtocol(MIFARE_CLASSIC, MIFARE_CLASSIC);
+
+    logger->info("=============== UseCase Generic #2: protocol based card selection " \
+                 "==================\n");
 
     /* Check if a card is present in the reader */
     if (!reader->isCardPresent()) {
@@ -93,17 +99,15 @@ int main()
         return 0;
     }
 
-    logger->info("= #### Select the card with no conditions\n");
+    logger->info("= #### Select the card if the protocol is '%'\n", MIFARE_CLASSIC);
 
     /* Get the core card selection manager */
     std::shared_ptr<CardSelectionManager> cardSelectionManager = 
         smartCardService.createCardSelectionManager();
 
-    /* 
-     * Create a card selection using the generic card extension without specifying any filter
-     * (protocol/power-on data/DFName).
-     */
-    std::shared_ptr<CardSelection> cardSelection = cardExtension.createCardSelection();
+    /*  Create a card selection using the generic card extension and specifying a Mifare filter. */
+    std::shared_ptr<GenericCardSelection> cardSelection = cardExtension.createCardSelection();
+    cardSelection->filterByCardProtocol(MIFARE_CLASSIC);
 
     /*
      * Prepare the selection by adding the created generic selection to the card selection scenario
@@ -116,24 +120,14 @@ int main()
 
     /* Check the selection result */
     if (selectionResult->getActiveSmartCard() == nullptr) {
-        throw IllegalStateException("The selection of the card failed.");
+        logger->error("The selection of the card failed\n");
+        return 0;
     }
 
     /* Get the SmartCard resulting of the selection */
     std::shared_ptr<SmartCard> smartCard = selectionResult->getActiveSmartCard();
 
     logger->info("= SmartCard = %\n", smartCard);
-
-    /* Execute an APDU to get CPLC Data (cf. Global Platform Specification) */
-    const std::vector<uint8_t> cplcApdu = ByteArrayUtil::fromHex("80CA9F7F00");
-
-    const std::vector<std::string> apduResponses =
-        cardExtension.createCardTransaction(reader, smartCard)
-                    ->prepareApdu(cplcApdu)
-                     .prepareReleaseChannel()
-                     .processApdusToHexStrings();
-
-    logger->info("CPLC Data: '%'\n", apduResponses[0]);
 
     logger->info("= #### End of the generic card processing\n");
 
